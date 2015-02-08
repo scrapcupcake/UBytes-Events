bab8945756aba6d0c3a3a791df0531a2840c3a6b
Hunter Thomas <ScrapCupcake@gmail.com>
2015-02-08 04:57:41 -0800

    Initial import

---
+++ Assets/CustomClickHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class CustomClickHandler : MonoBehaviour, IPointerClickHandler
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		void IWasClicked ()
+		{
+				Debug.Log ("I was hit by, struck by, a smooth mouse click in IWasClicked() on " + gameObject.name);
+		}
+	
+	#region IPointerClickHandler implementation
+		public void OnPointerClick (PointerEventData eventData)
+		{
+				Debug.Log (string.Format ("OnPointerClick, are you ok?\r\n{0} are you ok? Are you ok {0}?", gameObject.name));
+		}
+	#endregion
+}
